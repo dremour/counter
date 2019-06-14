80883e96a1efa193c865a2ca658937e80019f6f6
serhii <antonchyk68@gmail.com>
2019-06-14 15:07:49 +0300

    create counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const easeOutCubic = (currentIteration, changeInValue, totalIterations) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    changeInValue * (Math.pow(currentIteration / totalIterations - 1, 3) + 1)
   );
+};
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      maxValue: "100000",
+      currentIteration: 0
+    };
+  }
+
+  getCountUpdate = () => {
+    let { value, maxValue, currentIteration } = this.state;
+
+    const animationDuration = 3;
+    const fps = 60;
+    const totalIterations = animationDuration * fps;
+
+    let currentValue = Math.floor(
+      easeOutCubic(currentIteration, +maxValue, totalIterations)
+    );
+
+    if (value < +maxValue) {
+      requestAnimationFrame(this.getCountUpdate);
+    }
+
+    this.setState(prevState => ({
+      value: currentValue,
+      currentIteration: prevState.currentIteration + 1
+    }));
+  };
+
+  handleButton = () => {
+    this.getCountUpdate();
+    this.setState({
+      value: 0,
+      currentIteration: 0
+    });
+  };
+
+  handleInput = e => {
+    this.setState({
+      maxValue: e.target.value
+    });
+  };
+
+  render() {
+    let { value } = this.state;
+    return (
+      <div className="App">
+        <h1>Counter</h1>
+        <h3>{value}</h3>
+        <label>
+          What is the maximum value you want, Sir? Write please(optional,
+          default: 100000)
+        </label>
+        <input type="number" onChange={this.handleInput} />
+        <button onClick={this.handleButton}>
+          {value === 0 ? "Start" : "Reset"}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
